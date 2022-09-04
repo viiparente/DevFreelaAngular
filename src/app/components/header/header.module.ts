0dd9923d351504fc807d48b84d7f96f3eadc3d8a
Vinicius Parente <vd.parente@hotmail.com>
2022-09-04 17:31:36 -0300

    router component /header

--- src/app/components/header/header.module.ts
+++ src/app/components/header/header.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header.component';
+import { HeaderRoutingModule } from './header-routing.module';
 
 @NgModule({
   declarations: [
     HeaderComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    HeaderRoutingModule
   ],
   exports : []
 })
